70545f063d48264e1ad86b6c4733a2c22750fffe
Stuart Bloxham <s.bloxham@hotmail.co.uk>
2022-11-25 16:53:41 +0000

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "lists#index"
+
   resources :lists do
     resources :bookmarks
   end
   resources :bookmarks, only: [:destroy]
+  root "lists#index"
 end
